b42175daec5de82ff5d0670efa9a11bd8259224c
DanielLepszy <33004218+DanielLepszy@users.noreply.github.com>
2017-11-11 11:48:17 +0100

    Some fixes, remove unused arguments from methods, code refactoring

--- FunctionsofStandardCalculator.js
+++ FunctionsofStandardCalculator.js
@@ -1,130 +1,193 @@
-var simpleOperation;
+let simpleOperation;
 let l1;
 let l2;
+let keyValue;
 
-function clearCE(){
-	result.txt.value='0';
-	present=0;
+function clearCE() {
+	l1 = undefined;
+	l2 = undefined;
+	result.txt.value = '0';
+	present = 0;
 }
-function myZero(x){
-	z1=document.getElementById('number').value;
-	  if(z1=="0"||z1==undefined||z1==null||z1==NaN){
-	  result.txt.value="0";
-	  }
-	  else
-	  result.txt.value=result.txt.value+x;
-	  
+function myZero(keyValue) {
+	z1 = document.getElementById('number').value;
+	if (z1 == "0" || z1 == undefined || z1 == null || z1 == NaN) {
+		result.txt.value = "0";
 	}
-function diffrentZero(x){
-  
-	  if(document.getElementById('number').value=="0"){
-		  result.txt.value="";
-		  result.txt.value=result.txt.value+x;
-	  }
-	  
-	  else 
-	  result.txt.value=result.txt.value+x;
+	else
+		result.txt.value = result.txt.value + keyValue;
+
+}
+function diffrentZero(keyValue) {
+
+	if (document.getElementById('number').value == "0") {
+		result.txt.value = "";
+		result.txt.value = result.txt.value + keyValue;
+	}
+	else if (l1 != undefined && l2 != undefined) {
+		result.txt.value = "";
+		result.txt.value = keyValue;
+		l2 = keyValue;
+	}
+
+
+	else
+		result.txt.value = result.txt.value + keyValue;
 }
 
-function myOperations(x){
-	l1 = document.getElementById("number").value; 
-   
-	present=0;
-	result.txt.value='0';
-	simpleOperation=x;
-		
-}
+function operators(operators) {
 
-function myEqual(x){
-present=0;    
-l2 = document.getElementById("number").value;
-if (l1==undefined){
-	  result.txt.value=l2;    
-		   }
-else {
-	  result.txt.value=' ';
-		  switch (simpleOperation){
+
+	present = 0;
+	simpleOperation = operators;
+	if (l1 == undefined && l2 == undefined) {
+		l1 = document.getElementById("number").value;
+		result.txt.value = '0';
+	}
+	else if (l1 != undefined && l2 == undefined) {
+		l2 = document.getElementById("number").value;
+		result.txt.value = ' ';
+		present = 0;
+		switch (simpleOperation) {
 			case "+":
-			document.result.txt.value=((parseFloat(l1)+parseFloat(l2))); break;
+				document.result.txt.value = ((parseFloat(l1) + parseFloat(l2))); break;
 			case "-":
-			document.result.txt.value=((parseFloat(l1)-parseFloat(l2))); break;
+				document.result.txt.value = ((parseFloat(l1) - parseFloat(l2))); break;
 			case "*":
-			document.result.txt.value=((parseFloat(l1)*parseFloat(l2))); break;
+				document.result.txt.value = ((parseFloat(l1) * parseFloat(l2))); break;
 			case "/":
-				if(l2==0){
-				 alert("DIVISION NOT POSSIBLE"); break;}
-			  else{
-				document.result.txt.value=((parseFloat(l1)/parseFloat(l2))); break;}
-								  }
-	  }
+				if (l2 == 0) {
+					alert("DIVISION NOT POSSIBLE"); break;
+				}
+				else {
+					document.result.txt.value = ((parseFloat(l1) / parseFloat(l2))); break;
+				}
 
-						  }
-function arrowDelete(x) {
-  var delNumber = document.getElementById("number").value;
-  if (present==0){
-  delNumber=parseFloat(delNumber-(delNumber%10))/10;
-  result.txt.value=delNumber;
-  }
-  else if (present==1){
-	  var delDecimal=delNumber.substring(-delNumber.length,delNumber.length-1);
-	  result.txt.value=parseFloat(delDecimal);
-  }
+		}
+		l1 = document.getElementById("number").value;
+
+	}
+	else if (l1 != undefined && l2 != undefined) {
+		result.txt.value = keyValue;
+		result.txt.value = ' ';
+		switch (simpleOperation) {
+			case "+":
+				document.result.txt.value = ((parseFloat(l1) + parseFloat(l2))); break;
+			case "-":
+				document.result.txt.value = ((parseFloat(l1) - parseFloat(l2))); break;
+			case "*":
+				document.result.txt.value = ((parseFloat(l1) * parseFloat(l2))); break;
+			case "/":
+				if (l2 == 0) {
+					alert("DIVISION NOT POSSIBLE"); break;
+				}
+				else {
+					document.result.txt.value = ((parseFloat(l1) / parseFloat(l2))); break;
+				}
+		}
+		l1 = document.getElementById("number").value; 
+
+	} 
 }
 
-function plusMinus(x){
-  present=0;
-p1=document.getElementById("number").value;
-  if (p1==0){
-	  document.result.txt.value=p1;
-  }
-  else {
-	  document.result.txt.value=-1*p1;
-  }
+function equalSymbol() {
+	present = 0;
+	l2 = document.getElementById("number").value;
+	if (l1 == undefined) {
+		result.txt.value = l2;
+	}
+	else {
+		result.txt.value = ' ';
+		switch (simpleOperation) {
+			case "+":
+				document.result.txt.value = ((parseFloat(l1) + parseFloat(l2))); break;
+			case "-":
+				document.result.txt.value = ((parseFloat(l1) - parseFloat(l2))); break;
+			case "*":
+				document.result.txt.value = ((parseFloat(l1) * parseFloat(l2))); break;
+			case "/":
+				if (l2 == 0) {
+					alert("DIVISION NOT POSSIBLE"); break;
+				}
+				else {
+					document.result.txt.value = ((parseFloat(l1) / parseFloat(l2))); break;
+				}
+		}
+	}
+
+}
+function arrowDelete() {
+	var delNumber = document.getElementById("number").value;
+	if (present == 0) {
+		delNumber = parseFloat(delNumber - (delNumber % 10)) / 10;
+		result.txt.value = delNumber;
+	}
+	else if (present == 1) {
+		var delDecimal = delNumber.substring(-delNumber.length, delNumber.length - 1);
+		result.txt.value = parseFloat(delDecimal);
+	}
 }
 
-function oneBy(x){
-  present=0;
-  o1=document.getElementById("number").value;
-  if (o1==0){
-	  alert("DIVISION NOT POSSIBLE");
-  }
-  else {
-	  document.result.txt.value=1/l1;
-  }
+function positiveOrNegative() {
+	present = 0;
+	p1 = document.getElementById("number").value;
+	if (p1 == 0) {
+		document.result.txt.value = p1;
+	}
+	else {
+		document.result.txt.value = -1 * p1;
+	}
 }
-function mySqrt(z) {
-present=0;
-s1=document.getElementById("number").value;
 
-  if ( s1>=0 ){
-  result.txt.value=' ';
-  var Sqrte=Math.sqrt(s1);
-  document.result.txt.value= Sqrte;
+function oneByValue() {
+	present = 0;
+	o1 = document.getElementById("number").value;
+	if (o1 == 0) {
+		alert("DIVISION NOT POSSIBLE");
+	}
+	else {
+		document.result.txt.value = 1 / o1;
+	}
+}
+function theSecondRoot() {
+	present = 0;
+	s1 = document.getElementById("number").value;
 
-  }
-  else{
-  alert("NEGATIVE VALUE !");
-  }
- 
+	if (s1 >= 0) {
+		result.txt.value = ' ';
+		var Sqrte = Math.sqrt(s1);
+		document.result.txt.value = Sqrte;
+
+	}
+	else {
+		alert("NEGATIVE VALUE !");
+	}
+
 }
 var present;
 
-function dotSymbol(x){
-  present++
-  y1=document.getElementById("number").value;
- 
-	  if (present==1||present==0){
-		  result.txt.value=result.txt.value+x;
-	  }
-	  else if (present > 1){
-		  result.txt.value=y1;   
-	  }  
+function symbolDot(dot) {
+	present++
+	y1 = document.getElementById("number").value;
+
+	if (present == 1 || present == 0) {
+		result.txt.value = result.txt.value + dot;
+	}
+	else if (present > 1) {
+		result.txt.value = y1;
+	}
 }
 function isNumberKey(evt) {
-  var charCode = (evt.which) ? evt.which : event.keyCode;
-  if (charCode != 46 && charCode > 31
-  && (charCode < 48 || charCode > 57))
-	  return false;
+	var charCode = (evt.which) ? evt.which : event.keyCode;
+	if (charCode != 46 && charCode > 31
+		&& (charCode < 48 || charCode > 57))
+		return false;
 
-  return true;
+	return true;
 }
+function percentage() {
+	var parcentageValue = document.getElementById("number").value;
+	result.txt.value = ' ';
+	result.txt.value = parcentageValue / 100;
+}
+
